4024fd48b66fbd896f456534ebec8744cd3c4210
Wildan S. Nahar <winistein@gmail.com>
2014-12-12 20:32:39 +0700

    change password feature

---
+++ change-pass.php
@@ -0,0 +1,17 @@
+<head> 
+	<title>
+		Pemilu ARC 2014 - Sign up
+	</title>
+	<link rel="icon" 
+      type="image/png" 
+      href="img/pemilu-arc.png">
+      <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body>
+	<div class="main">
+<?php include('header.php'); ?>
+<?php include('side-content-change-pass.php'); ?>
+<?php include('footer.php'); ?>
+	</div>
+</body>
+</html>
