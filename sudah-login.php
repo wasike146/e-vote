ae1e3197611184d6aa164d326f675582534da2d1
Wildan S. Nahar <winistein@gmail.com>
2014-12-16 15:59:53 +0700

    Feature for has logged

---
+++ sudah-login.php
@@ -0,0 +1,22 @@
+<body>
+<head>
+	<style type="text/css">
+	.login  {
+		font-size: 16px;
+		text-align: center;
+		
+	}
+
+	.login strong {
+		color: red;
+		padding-top: 2px;
+	}
+	</style>
+</head>
+<br>
+	<div class="login">
+	
+		<strong> ANDA SUDAH LOG IN </strong> <br><br>
+			
+	</div>
+</body>
